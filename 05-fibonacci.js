9b18d6634bdcd118d8e533b8cee5a97110c4d5ae
Geovani <geovanyferreiraa@gmail.com>
2024-03-21 20:01:18 -0300

    Segunda aula

---
+++ 05-fibonacci.js
@@ -0,0 +1,13 @@
+//Calculo de Fibonacci 
+const fibonacci = [];
+
+fibonacci[1] = 1;
+fibonacci[2] = 1;
+
+for (let i = 3; i < 20; i++) {
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];    
+}
+
+for (let i = 0; i < fibonacci.length; i++) {
+    console.log(fibonacci[i]);
+}
